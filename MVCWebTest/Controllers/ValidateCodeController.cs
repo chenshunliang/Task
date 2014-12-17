4abb5fcea86d9a4296e8c8f8e878458b65ccd2e0
chenshunliang <zxcvbnm1>
2014-12-18 00:06:51 +0800

    ceshi

--- MVCWebTest/Controllers/ValidateCodeController.cs
+++ MVCWebTest/Controllers/ValidateCodeController.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVCWebTest.Tools;
-using Webdiyer.WebControls.Mvc;
+using MVCWebTest.maipuhui;
+using System.Collections.Specialized;
 
 namespace MVCWebTest.Controllers
 {
@@ -13,13 +14,32 @@ namespace MVCWebTest.Controllers
         //
         // GET: /ValidateCode/
 
+        private static string json = "";
+
         public ActionResult Index()
         {
+            NameValueCollection param = Request.Form;
+
+            ExamPeople peo = new ExamPeople();
+            peo.assessments_id = 1;
+            peo.examination_id = 111;
+            peo.company.company_id = 2;
+            peo.company.company_name = "测试公司";
+            peo.company.apply_post = "aaa";
+            peo.company.apply_post_id = 1212;
+            peo.resume.education = "asdas";
+            peo.resume.email = "asdasdsa";
+            peo.resume.fullname = "asda";
+            peo.resume.professional = "sadasdasdasd";
+            peo.resume.resume_id = 122;
+
+            json = JSONHelper.ObjectToJSON(peo);
             return View();
         }
 
         public ActionResult ValiCode()
         {
+
             ValiCode vCode = new ValiCode();
             string code = vCode.CreateValidateCode(5);
             Session["ValidateCode"] = code;
@@ -29,6 +49,8 @@ namespace MVCWebTest.Controllers
 
         public ActionResult Vali()
         {
+            MaiPH.WRequest("http://localhost:2378/ValidateCode/index", "post", json);
+
             string code = Request["Code"].Trim();
             string codeSess = Session["ValidateCode"].ToString();
             if (code.Equals(codeSess))
